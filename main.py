44257b2320ce7af18e9bbc240c58a6911bf4cf7a
YasiruShaminda <yasiru.20200496@iit.ac.lk>
2022-02-22 23:55:05 +0530

    All Done with clear code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def get_likes():
+def get_likes(file):
     try:
-        with open("e_elaborate.in.txt", 'r') as f:
+        with open(file, 'r') as f:
             lines = f.readlines()
 
             for line in range(1, len(lines), 2):
@@ -16,9 +16,9 @@ def get_likes():
         return "File not found"
 
 
-def get_dislikes():
+def get_dislikes(file):
     try:
-        with open("b_basic.in.txt", 'r') as f:
+        with open(file, 'r') as f:
             lines = f.readlines()
 
             for line in range(2, len(lines), 2):
@@ -34,25 +34,31 @@ def get_dislikes():
         return "File not found"
 
 
+def output():
+    textfile = open("output.txt", "w")
+
+    likesCount = len(all_likes)
+
+    textfile.write("{} ".format(likesCount))
+    for element in all_likes:
+        textfile.write(element + " ")
+    textfile.close()
+
+
+file = "e_elaborate.in.txt"
 all_likes = []
 all_dislikes = []
 
-get_likes()
-get_dislikes()
-
+get_likes(file)
 all_likes = list(dict.fromkeys(all_likes))
+
+get_dislikes(file)
 all_dislikes = list(dict.fromkeys(all_dislikes))
 
 for element in all_dislikes:
     if element in all_likes:
         all_likes.remove(element)
 
+output()
 
-textfile = open("output.txt", "w")
-
-likesCount = len(all_likes)
-textfile.write("{} ".format(likesCount))
-for element in all_likes:
-    textfile.write(element + " ")
-textfile.close()
-print("Job Done")
+print("Your output is ready!!!")
