5c064ed152d6499e96f3dd8b010b703d8dddcd55
Chevon Fernando <chevonfernando10@gmail.com>
2022-02-22 23:06:34 +0530

    retrived

--- PracticeRound/__init__.py
+++ PracticeRound/__init__.py
@@ -1,31 +1,23 @@
 def read_file():
     try:
-        count = 0
         with open("a_an_example.in.txt", 'r') as f:
             lines = f.readlines()
             print(lines)
 
             for line in range(1, len(lines), 2):
-                # count += 1
                 likes = lines[line].strip().split()
 
-                # for x in (1, len(likes)):
-                # # if x < 0:
-                # # like_f = likes[x]
-                #     all_likes = []
-                #     all_likes.append(likes[x])
+                likes.pop(0)
+                for x in  range(len(likes)):
+                    all_likes.append(likes[x])
 
                 print(likes)
-
-                print("Line {}: {}".format(line, likes[1]))
-                # print(all_likes)
-
-                # if (count / 2) == int(count / 2):
-                #     for x in range(1, len(line)):
-                #         print("Line {}: {}".format(count, line.strip()))
+                print("Line {}: {}".format(line, likes))
+                print(all_likes)
 
     except FileNotFoundError:
         return "File not found"
 
-
+all_likes = []
 read_file()
+
